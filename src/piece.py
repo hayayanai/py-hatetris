8ab58833b21074d00d74a0ad923306c5b170763c
hayayanai <yanaihayato8718810@gmail.com>
2022-09-16 02:12:49 +0900

    Add Mino Enum

--- src/piece.py
+++ src/piece.py
@@ -1,7 +1,17 @@
+from enum import Enum
 from typing import Literal
 
 
-class Piece:
+class Mino(Enum):
+    I = 0
+    J = 1
+    L = 2
+    O = 3
+    S = 4
+    T = 5
+    Z = 6
+
+class Piece():
     UNMODIFIED: dict[str, list] = {
         "I": [
             [
